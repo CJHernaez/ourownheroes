a2bde41de6829aea7deae49148798cd52d485aa2
CJ Hernaez <cjhernaez@me.com>
2023-03-18 03:05:19 -0400

    i know committing keys is a terrible idea. ill invalidate them when i figure it out

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SetStateAction, useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [lightStatus, setLightStatus] = useState(undefined);
+
+  useEffect(() => {}, []);
+
+  const toggleLight = () => {
+    axios
+      .request({
+        method: "POST",
+        url: `https://api.lifx.com/v1/lights/id%3Ad073d5656598/toggle`,
+        headers: {
+          accept: "application/json",
+          "content-type": "application/json",
+          Authorization:
+            "Bearer c3e80b048bc86f0281665e3be8995ae2788eabb6ddc0b0b2cb57259a0c1172f7",
+        },
+        data: { duration: 1 },
+      })
+      .then(function (response) {
+        console.log(response.data);
+        setLightStatus(response.data.results[0].power);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+
+    console.log(lightStatus);
+    return count;
+  };
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://api.lifx.com/v1/lights/id%3Ad073d5656598",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer c3e80b048bc86f0281665e3be8995ae2788eabb6ddc0b0b2cb57259a0c1172f7",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setLightStatus(response.data[0].power);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <div className="App">
@@ -18,21 +69,16 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={toggleLight}>
+          {" "}
+          {lightStatus ? `The lights are ${lightStatus}` : "Checking Status"}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>Environment: {process.env.NODE_ENV}</p>
       </div>
-      <p className="read-the-docs">
-        v2
-      </p>
       <p className="cj-throws-stuff-together">
-        CJ will learn AWS through this... probably. a
+        CJ will learn AWS through this... probably.
       </p>
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
